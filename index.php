4972628b623075a9c03f85666dd96c0aa96600df
Pratik <pratikpatel8889@gmail.com>
2021-02-01 09:38:04 +0530

    removed code

--- index.php
+++ index.php
@@ -1,23 +1,3 @@
 <?php
-
-echo "testing"
-
-
-echo "testing git push";
-echo "<br/>";
-echo "Added new changes";
-
-
-
-
-
-
-echo "tag created and started now";
-
-
-
-echo "added development changes for testing";
-
-
-
+echo "final branch changes";
 ?>
